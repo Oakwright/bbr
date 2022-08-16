c79830234188a415f1ecaa801e0f7a6305e45fc3
Abby Possum <97874630+Oakwright@users.noreply.github.com>
2022-08-15 21:08:57 -0700

    maybe working who knows

--- bbrsite/bbrsite/settings/dev.py
+++ bbrsite/bbrsite/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z8idcfi9ywxj4n1!hg5ecu$xfp@s2d16=8i)6aqoq&_y_p_64)'
+SECRET_KEY = 'django-insecure-%9e0-kff1xxu6c&3s2mp4dx8jl6f)hx$&&#tia+i0m1n-il3gp'
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
--- bbrsite/bbrsite/urls.py
+++ bbrsite/bbrsite/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls import url
-
 from django.urls import include, path
 from django.contrib import admin
 
